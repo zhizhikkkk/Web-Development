0162f56e967ddf3b2fc86dba27839ad8e6a7425a
zhizhikkkk <zhanmamyt@bk.ru>
2023-02-22 16:41:37 +0600

    Lab4

---
+++ Lab4/LearningTypescript/BasicTypes.ts
@@ -0,0 +1,20 @@
+const isFetching : boolean = false
+const isLoading : boolean = true
+
+const int: number = 42
+const float : number = 4.2
+const num: number = 3e10
+
+const message : string = 'Hello Typescript'
+
+const numberArray:number[] = [1,2,3,4,5,6,7,8,9]
+const numberArray2:Array<number> =[1,2,3,4,5,6,7,8,9]
+
+const words: string []= ['Hello','Typescript']
+
+//Tuple
+const contact:[string,number] =['Vasya',1234567]
+
+//Any
+let variable = 42
+
